92706fbe16f0fdba5592c0d4e9596a65203e09f7
Renaataa <49534519+Renaataa@users.noreply.github.com>
2020-12-08 20:41:17 +0100

    ng-bind ng-model ng-change ng-click

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,33 @@
-var app = angular.module('MyWebsite', []);
+var app = angular.module('MyWebsite', [])
 
-app.controller('Ctrl', [ function(){
+app.controller('Ctrl', ['$http', function($http){
     console.log('Kontroler Ctrl startuje')
+    var ctrl = this
+
+    ctrl.newPerson = {
+        firstName: '',
+        lastName: '',
+        yearOfBirth: 2000
+    }
+
+    $http.get('/get').then(
+        function(res){
+            ctrl.person = res.data
+        },
+        function(err){}
+    )
+
+    ctrl.dataChanged = function(){
+        $http.get('/set?firstName=' + ctrl.person.firstName + '&lastName=' + ctrl.person.lastName + '&yearOfBirth=' + ctrl.person.yearOfBirth).then(
+            function(res){},
+            function(err){}
+        )
+    }
+
+    ctrl.sendNewData = function(){
+        $http.get('/set?firstName=' + ctrl.newPerson.firstName + '&lastName=' + ctrl.newPerson.lastName + '&yearOfBirth=' + ctrl.newPerson.yearOfBirth).then(
+            function(res){ctrl.person=res.data},
+            function(err){}
+        )
+    }
 }])
